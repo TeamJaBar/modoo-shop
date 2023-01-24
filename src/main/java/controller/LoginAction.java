6af20fb817e426a2432248e8fd143bd199db92c7
Subin Jo <subiiin6085@naver.com>
2023-01-25 03:45:46 +0900

    Fix:"DB에 저장되지 않은 아이디 비밀번호 입력시 에러 페이지로 넘어가는 버그 fix"

--- src/main/java/controller/LoginAction.java
+++ src/main/java/controller/LoginAction.java
@@ -11,8 +11,6 @@ public class LoginAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ActionForward forward = new ActionForward();
-		forward.setPath("main.do");
-		forward.setRedirect(false);
 
 		MemberDAO mdao = new MemberDAO();
 		MemberVO mvo = new MemberVO();
@@ -20,8 +18,16 @@ public class LoginAction implements Action {
 		mvo.setmPw(request.getParameter("mPw"));
 		MemberVO member = mdao.selectOneLogin(mvo);
 
-		request.getSession().setAttribute("mId", member.getmId());
-		request.getSession().setAttribute("mName", member.getmName());
+		if(member == null) {
+			response.setContentType("text/html; charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");  
+			response.getWriter().println("<script>alert('회원정보가 없습니다. 아이디 또는 비밀번호를 확인해주세요.'); history.go(-1);</script>");
+		} else {
+			request.getSession().setAttribute("mId", member.getmId());
+			request.getSession().setAttribute("mName", member.getmName());
+			forward.setPath("main.do");
+			forward.setRedirect(false);
+		}
 
 		return forward;
 	}
