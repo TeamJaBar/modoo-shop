b62cdd24360adb5a9799b962b850e9cb24d3fe1c
kyeon1 <rkdussla444@naver.com>
2023-01-26 20:07:28 +0900

    Feat : "찜 리스트 추가&삭제 controller 추가"

---
+++ src/main/java/controller/DeleteDibController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.DibDAO;
+import member.DibVO;
+
+/**
+ * Servlet implementation class DeleteDibController
+ */
+@WebServlet("/view/dibDelete")
+public class DeleteDibController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public DeleteDibController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DibVO dvo=new DibVO();
+		DibDAO ddao=new DibDAO();
+		
+		dvo.setDibNum(Integer.parseInt(request.getParameter("dibNum")));
+		
+		if(ddao.delete(dvo)) {
+			response.getWriter().println(1);
+		}
+	}
+
+}
---
+++ src/main/java/controller/InsertDibController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.DibDAO;
+import member.DibVO;
+
+/**
+ * Servlet implementation class InsertDibController
+ */
+@WebServlet("/view/dibInsert")
+public class InsertDibController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public InsertDibController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response, Object mNum) throws ServletException, IOException {
+		DibVO dvo=new DibVO();
+		DibDAO ddao=new DibDAO();
+		
+		dvo.setpNum(Integer.parseInt(request.getParameter("pNum")));
+		dvo.setmNum((int)(request.getSession().getAttribute("mNum")));
+		
+		if(ddao.insert(dvo)) {
+			response.getWriter().println(1);
+		}
+		
+		
+	}
+
+}
