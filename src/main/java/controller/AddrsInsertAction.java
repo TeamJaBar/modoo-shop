0ca904f2afc8b23e70fa0f47266a2a7a007a5e6d
polodumbo <polosh1208@gmail.com>
2023-01-27 14:28:24 +0900

    feat: 컨트롤러 - 배송지 AddrsSelectAcion(SelectAll + SelectOne), AddrsInsertAction 추가

---
+++ src/main/java/controller/AddrsInsertAction.java
@@ -0,0 +1,42 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.AddressDAO;
+import member.AddressVO;
+
+public class AddrsInsertAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ActionForward forward = new ActionForward();
+
+		forward.setPath("addrsSelect.do");
+		forward.setRedirect(true);
+
+		AddressDAO adao = new AddressDAO();
+		AddressVO avo = new AddressVO();
+
+		avo.setShipName(request.getParameter("shipName"));
+		avo.setDestination(request.getParameter("destination"));
+		avo.setZipCode(request.getParameter("zipCode"));
+		avo.setUserAddr(request.getParameter("userAddr"));
+		avo.setDetailAddr(request.getParameter("detailAddr"));
+		avo.setTel(request.getParameter("tel"));
+		avo.setmNum((int)request.getSession().getAttribute("mNum"));
+		
+		if (request.getParameter("defaultFl") == null) {
+			avo.setIsDefault("0"); // 일반
+		} else {
+			adao.update(avo); // 기존 기본배송지 일반배송지로
+			avo.setIsDefault("1");
+		}
+
+		if (adao.insert(avo)) {
+			return forward;
+		}
+
+		return null;
+	}
+}
