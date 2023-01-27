c0be42f287f875e41966956dab5a9826f02073f9
Subin Jo <subiiin6085@naver.com>
2023-01-27 10:00:31 +0900

    Feat : "QuickViewController : 퀵뷰 ajax 요청 처리하는 컨트롤러 추가, dib.tag : 찜 추가 커스텀태그 추가"

---
+++ src/main/java/controller/QuickViewController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import product.ProductDAO;
+import product.ProductVO;
+
+@WebServlet("/view/quickView")
+public class QuickViewController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public QuickViewController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductDAO pdao = new ProductDAO();
+		ProductVO pvo = new ProductVO();
+		
+		int pNum  = Integer.parseInt(request.getParameter("pNum"));
+		pvo.setpNum(pNum);
+		
+		pvo = pdao.selectOne(pvo);
+		
+		if(pvo != null) {
+			Gson gson = new Gson();
+			response.setContentType("text/html;charset=UTF-8"); 
+			response.getWriter().print(gson.toJson(pvo));
+			System.out.println(gson.toJson(pvo));
+		}
+	}
+}
