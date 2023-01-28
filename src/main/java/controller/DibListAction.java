3421c46b4f9bde04c0a31d5441827ea94ec5ace8
Subin Jo <subiiin6085@naver.com>
2023-01-28 23:31:51 +0900

    Feat : "찜 목록 컨트롤러 추가"

---
+++ src/main/java/controller/DibListAction.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.DibDAO;
+import member.DibVO;
+
+public class DibListAction implements Action{
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ActionForward forward = new ActionForward();
+		forward.setPath("dibs-list.jsp");
+		forward.setRedirect(false);
+		
+		DibDAO ddao = new DibDAO();
+		DibVO dvo = new DibVO();
+		
+		if(request.getSession().getAttribute("mNum")!= null) {
+			dvo.setmNum((int)request.getSession().getAttribute("mNum"));
+		}
+		
+		request.setAttribute("dList", ddao.selectAll(dvo));
+		
+		return forward;
+	}
+
+}
