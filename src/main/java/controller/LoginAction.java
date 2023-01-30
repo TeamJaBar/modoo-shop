2fbc0387ed1682f24f51a8fa530a2418b4a9c5d7
Subin Jo <subiiin6085@naver.com>
2023-01-30 21:52:28 +0900

    Feat : "로그인 아이디 유지 쿠키 컨트롤러 수정 및 뷰 추가"

--- src/main/java/controller/LoginAction.java
+++ src/main/java/controller/LoginAction.java
@@ -23,24 +23,29 @@ public class LoginAction implements Action {
 		MemberVO member = mdao.selectOneLogin(mvo);
 		
 		String saveId=request.getParameter("saveId");
+		
+		System.out.println("체크박스 : " + saveId);
+		
 		Cookie cookie=new Cookie("mId",mId);
 
-		if(saveId !=null) {//saveId가 있을때(아이디저장 체크가 되어있을때)
-			request.setCharacterEncoding("UTF-8");
-			cookie.setMaxAge(60*60*24*30);
-			response.addCookie(cookie);
-			return forward;
-		} else {//체크가 되어있지 않을때
+		
+		if(member == null) {
 			cookie.setMaxAge(0);
 			response.addCookie(cookie);
-		}
-		if(member == null) {
 			forward.setRedirect(false);
 			forward.setPath("alert.jsp");		
 			request.setAttribute("title", "가입된 정보가 없습니다.");
 			request.setAttribute("msg", "아이디 혹은 비밀번호를 확인하세요.");
 			request.setAttribute("url", "login.jsp");
 		} else {
+			//로그인 성공
+			if(saveId !=null) {//saveId가 있을때(아이디저장 체크가 되어있을때)
+				cookie.setMaxAge(60*60*24*30);
+				response.addCookie(cookie);
+			} else {//체크가 되어있지 않을때
+				cookie.setMaxAge(0);
+				response.addCookie(cookie);
+			}
 			request.getSession().setAttribute("mNum", member.getmNum());
 			request.getSession().setAttribute("mId", member.getmId());
 			request.getSession().setAttribute("mName", member.getmName());
